f350238a7325ea9734c27e8f7af85ead4a98aa4e
Nattie14 <166996757+Nattie14@users.noreply.github.com>
2024-04-26 22:47:11 +0100

    Added the java script functionality to search for a city and temp

--- src/script.js
+++ src/script.js
@@ -1,9 +1,28 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input"); 
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+  
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "ba4608fb3t14e3e4af50o5a7601d9309";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city"); 
+  
     cityElement.innerHTML = searchInput.value;
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
