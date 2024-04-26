d67fd4218f9e5ce0f58d02de270ebc93279c91aa
Nattie14 <166996757+Nattie14@users.noreply.github.com>
2024-04-27 00:19:03 +0100

    Injected more weather data to fetch correct date/time

--- src/script.js
+++ src/script.js
@@ -2,9 +2,32 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+  
+    console.log(response.data);
   
     temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+  }
+  
+  function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+    let day = days[date.getDay()];
+    let month = months[date.getMonth()];
+  
+    return `${day}, ${month} ${date.getDate()} ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
   }
   
   function searchCity(city) {
@@ -17,7 +40,7 @@ function refreshWeather(response) {
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city"); 
+    let cityElement = document.querySelector("#city");
   
     cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
@@ -25,6 +48,6 @@ function refreshWeather(response) {
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
- 
+  
   searchCity("Nyazura");
   
