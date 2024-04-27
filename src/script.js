6ac09f5094d2e9e561afbbbabd064c04f6d7df38
Nattie14 <166996757+Nattie14@users.noreply.github.com>
2024-04-27 10:25:37 +0100

    Updated the java script to display correct icon image & start the description with a capital letter

--- src/script.js
+++ src/script.js
@@ -7,18 +7,25 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-  
-    console.log(response.data);
+    let iconElement = document.querySelector("#icon");
+   
   
     temperatureElement.innerHTML = Math.round(temperature);
-    descriptionElement.innerHTML = response.data.condition.description;
+    descriptionElement.innerHTML = capitalizeFirstLetter(response.data.condition.description);
     cityElement.innerHTML = response.data.city;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
-  }
-  
-  function formatDate(date) {
+    iconElement.innerHTML =  `<img src="${response.data.condition.icon_url}"
+    class="weather-app-icon" 
+    />`
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function formatDate(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -28,26 +35,27 @@ function refreshWeather(response) {
     let month = months[date.getMonth()];
   
     return `${day}, ${month} ${date.getDate()} ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-  }
-  
-  function searchCity(city) {
+}
+
+function searchCity(city) {
     let apiKey = "ba4608fb3t14e3e4af50o5a7601d9309";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   
     axios.get(apiUrl).then(refreshWeather);
-  }
-  
-  function handleSearchSubmit(event) {
+}
+
+function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
   
     cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
-  }
-  
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
-  
-  searchCity("Nyazura");
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nyazura");
+
   
